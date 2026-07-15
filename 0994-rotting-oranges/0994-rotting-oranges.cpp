cc6dc2102871f7d4ecd07f94ce1d4eedaea77e0d
zeroTrace <142493670+0xZ15H4N@users.noreply.github.com>
2026-07-15 19:17:09 +0530

    3 ms (26.56%) 17.3 MB (17.51%) 07-15-2026 0994-rotting-oranges Medium

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,8 +3,10 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
+        // considering queue with pair which contain a pair and timer
+        queue<pair<pair<int,int>,int>>q;  
         vector<vector<int>> vis(rows,vector<int>(cols,0));
+        // pushing all the rotten oranges in the queue
         for(int i = 0 ; i < rows;i ++){
             for(int j = 0; j < cols; j++){
                 if(grid[i][j] == 2){
@@ -12,7 +14,7 @@ public:
                 }
             }
         }
-
+        // only tracks the max Time
         int timer = 0;
         vector<int> dirRow = {-1,0,1,0};
         vector<int> dirCol = {0,-1,0,1};
@@ -21,11 +23,12 @@ public:
             int col = q.front().first.second;
             int tm = q.front().second;
             timer = max(timer,tm);
+            vis[row][col] = 1;
             q.pop();
             for(int i= 0; i < 4 ; i++){
                 int nRow = row + dirRow[i];
                 int nCol = col + dirCol[i];
-                if(nRow >= 0 && nRow < rows && nCol >=0 && nCol < cols &&!vis[nRow][nCol] && grid[nRow][nCol] == 1){
+                if(nRow >= 0 && nRow < rows && nCol >=0 && nCol < cols && !vis[nRow][nCol] && grid[nRow][nCol] == 1){
                     vis[nRow][nCol] = 1;
                     grid[nRow][nCol] = 2;
                     q.push(make_pair(make_pair(nRow,nCol),tm+1));
