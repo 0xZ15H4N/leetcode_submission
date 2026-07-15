64df8c9d1adbfc2d2bdc590f5ab63fc2a461eeb5
zeroTrace <142493670+0xZ15H4N@users.noreply.github.com>
2026-07-15 19:21:59 +0530

    0 ms (100%) 16.8 MB (70.36%) 07-15-2026 0994-rotting-oranges Medium

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
@@ -5,18 +9,22 @@ public:
         int cols = grid[0].size();
         // considering queue with pair which contain a pair and timer
         queue<pair<pair<int,int>,int>>q;  
-        vector<vector<int>> vis(rows,vector<int>(cols,0));
+
         // pushing all the rotten oranges in the queue
+        int count = 0;
         for(int i = 0 ; i < rows;i ++){
             for(int j = 0; j < cols; j++){
                 if(grid[i][j] == 2){
                     q.push(make_pair(make_pair(i,j),0));
-                    vis[i][j] = 1;
+                }
+                if(grid[i][j] == 1){
+                    count++;
                 }
             }
         }
         // only tracks the max Time
         int timer = 0;
+        int cntFresh = 0;
         vector<int> dirRow = {-1,0,1,0};
         vector<int> dirCol = {0,-1,0,1};
         while(!q.empty()){
@@ -28,20 +36,16 @@ public:
             for(int i= 0; i < 4 ; i++){
                 int nRow = row + dirRow[i];
                 int nCol = col + dirCol[i];
-                if(nRow >= 0 && nRow < rows && nCol >=0 && nCol < cols && !vis[nRow][nCol] && grid[nRow][nCol] == 1){
-                    vis[nRow][nCol] = 1;
+                if(nRow >= 0 && nRow < rows && nCol >=0 && nCol < cols &&grid[nRow][nCol] == 1){
                     grid[nRow][nCol] = 2;
                     q.push(make_pair(make_pair(nRow,nCol),tm+1));
+                    cntFresh++;
                 }
             }
             
         }
-        for(int i = 0 ; i < rows;i ++){
-            for(int j = 0; j < cols; j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
+        if(count != cntFresh){
+            return -1;
         }
         
         return timer;
