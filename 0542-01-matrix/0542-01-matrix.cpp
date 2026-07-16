284e8838584e0efc814c96ac37adf5f8d5b96ae2
zeroTrace <142493670+0xZ15H4N@users.noreply.github.com>
2026-07-16 14:09:22 +0530

    9 ms (89.32%) 39 MB (11.77%) 07-16-2026 0542-01-matrix Medium

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
-    vector<int> DIR = {0, 1, 0, -1, 0};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
-        queue<pair<int, int>> q;
-        for (int r = 0; r < m; ++r)
-            for (int c = 0; c < n; ++c)
-                if (mat[r][c] == 0) q.emplace(r, c);
-                else mat[r][c] = -1; // Marked as not processed yet!
-
-        while (!q.empty()) {
-            auto [r, c] = q.front(); q.pop();
-            for (int i = 0; i < 4; ++i) {
-                int nr = r + DIR[i], nc = c + DIR[i+1];
-                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
-                mat[nr][nc] = mat[r][c] + 1;
-                q.emplace(nr, nc);
+        int rows = mat.size();
+        int cols = mat[0].size();
+        vector<vector<int>> dist(rows,vector<int>(cols,0));
+        vector<vector<int>> vis(rows,vector<int>(cols,0));
+        queue<pair<pair<int,int>,int>>q;
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(mat[i][j] == 0){
+                    q.push({{i,j},0});
+                    vis[i][j] = 1;
+                }
             }
         }
-        return mat;
+        int delRow[] = {-1,0,1,0};
+        int delCol[] = {0,-1,0,1};
+
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+
+            dist[row][col] = steps;
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int nRow  = row + delRow[i];
+                int nCol  = col + delCol[i];
+                if(nRow >=0 && nRow < rows && nCol >=0 && nCol < cols && vis[nRow][nCol] == 0){
+                    q.push({{nRow,nCol},steps+1});
+                    vis[nRow][nCol] = 1;
+                }
+
+            }
+        }
+        return dist;
     }
 };
