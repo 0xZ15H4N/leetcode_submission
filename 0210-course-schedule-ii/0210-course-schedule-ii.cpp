ca3846d5477beabb71a1ba8b5202e3ef598afbb9
zeroTrace <142493670+0xZ15H4N@users.noreply.github.com>
2026-07-18 00:51:09 +0530

    3 ms (71.01%) 18.2 MB (71.18%) 07-18-2026 0210-course-schedule-ii Medium

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjLs(numCourses);
+        vector<int> inDegree(numCourses, 0);
+        queue<int> q;
+        vector<int> topo;
+
+        // created an adjency list
+        for (int i = 0; i < prerequisites.size(); i++) {
+            adjLs[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            for (auto it : adjLs[i]) {
+                inDegree[it]++;
+            }
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) { // no indegree
+                q.push(i);
+            }
+        }
+
+        while (!q.empty()) {
+            int top = q.front();
+            q.pop();
+            topo.push_back(top);
+            for (auto it : adjLs[top]) {
+                inDegree[it]--;
+                if (inDegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        if (topo.size() == numCourses) {
+            return topo; // no cycle detected
+        }
+        return {};
+    }
+};
