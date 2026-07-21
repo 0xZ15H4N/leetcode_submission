a80f4ad0b8fcefed4e85005ee7fe2f23c549bf78
zeroTrace <142493670+0xZ15H4N@users.noreply.github.com>
2026-07-22 01:09:14 +0530

    0 ms (100%) 18 MB (90.83%) 07-22-2026 0733-flood-fill Easy

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,21 @@
 class Solution {
 private:
-    void dfs(vector<vector<int>>&vis,vector<vector<int>>&image,int sr,int sc,int color,int iniColor){
-        if(image[sr][sc] == iniColor){
-            image[sr][sc] = color;
-        }
-        vis[sr][sc] = 1;
-        vector<int> dirRow = {-1,0,1,0};
-        vector<int> dirCol = {0,-1,0,1};
-        for(int step = 0; step < 4;step++){
-            int nRow = sr + dirRow[step];
-            int nCol = sc + dirCol[step];
-            if(nRow >= 0 && nRow <image.size() && nCol >=0 && nCol < image[0].size() && !vis[nRow][nCol] && image[nRow][nCol] == iniColor){
-                image[nRow][nCol] = color;
-                dfs(vis,image,nRow,nCol,color,iniColor);
+    void dfs(vector<vector<int>>&image,int sr,int sc,int color,int parent){
+        if(image[sr][sc] == color){return;}
+        image[sr][sc] = color;
+        int dir[] = {-1,0,1,0,-1};
+        for(int i = 1; i <= 4; i++){
+            int nrow = sr + dir[i-1];
+            int ncol = sc + dir[i];
+            if(nrow < image.size() && nrow >= 0 && ncol < image[0].size() && ncol >=0 && image[nrow][ncol] == parent){
+                dfs(image,nrow,ncol,color,parent);
             }
         }
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int iniColor = image[sr][sc];
-        vector<vector<int>> vis(image.size(),vector<int>(image[0].size(),0));
-        dfs(vis,image,sr,sc,color,iniColor);
-        return image;
+       int parent = image[sr][sc];
+       dfs(image,sr,sc,color,parent);
+       return image;
     }
 };
