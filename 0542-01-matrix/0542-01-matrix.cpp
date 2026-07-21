4350bb1342edf2e1048993060dd42bd7916a405b
zeroTrace <142493670+0xZ15H4N@users.noreply.github.com>
2026-07-22 00:33:04 +0530

    23 ms (40.19%) 38.8 MB (30.04%) 07-22-2026 0542-01-matrix Medium

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int rows = mat.size();
-        int cols = mat[0].size();
-        vector<vector<int>> dist(rows,vector<int>(cols,0));
-        vector<vector<int>> vis(rows,vector<int>(cols,0));
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>>q;
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
+        for(int i = 0; i< n; i++){
+            for(int j=0; j<m; j++){
                 if(mat[i][j] == 0){
                     q.push({{i,j},0});
                     vis[i][j] = 1;
                 }
             }
         }
-        int delRow[] = {-1,0,1,0};
-        int delCol[] = {0,-1,0,1};
-
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
-            int steps = q.front().second;
-
-            dist[row][col] = steps;
+            int step = q.front().second;
             q.pop();
-            for(int i = 0; i < 4; i++){
-                int nRow  = row + delRow[i];
-                int nCol  = col + delCol[i];
-                if(nRow >=0 && nRow < rows && nCol >=0 && nCol < cols && vis[nRow][nCol] == 0){
-                    q.push({{nRow,nCol},steps+1});
-                    vis[nRow][nCol] = 1;
+            mat[row][col] = step;
+            int dir[] = {-1,0,1,0,-1};
+            for(int i =1; i<= 4; i++){
+                int nrow = row + dir[i-1];
+                int ncol = col + dir[i];
+                if(ncol >= 0 && ncol < m && nrow >=0 && nrow < n && vis[nrow][ncol] == 0){
+                    q.push({{nrow,ncol},step+1});
+                    vis[nrow][ncol] = 1;
                 }
-
             }
         }
-        return dist;
+        return mat;
     }
 };
