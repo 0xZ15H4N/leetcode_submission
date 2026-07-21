b40fefea9f7552bee9ebc2f64acd185f7beeffc7
zeroTrace <142493670+0xZ15H4N@users.noreply.github.com>
2026-07-22 01:14:20 +0530

    4 ms (5.48%) 18.4 MB (20.56%) 07-22-2026 0733-flood-fill Easy

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,21 +1,23 @@
 class Solution {
 private:
-    void dfs(vector<vector<int>>&image,int sr,int sc,int color,int parent){
-        if(image[sr][sc] == color){return;}
+    void dfs(vector<vector<int>>&image,vector<vector<int>>&vis,int &sr,int &sc,int &color,int &parent){
+
         image[sr][sc] = color;
+        vis[sr][sc] = 1;
         int dir[] = {-1,0,1,0,-1};
         for(int i = 1; i <= 4; i++){
             int nrow = sr + dir[i-1];
             int ncol = sc + dir[i];
-            if(nrow < image.size() && nrow >= 0 && ncol < image[0].size() && ncol >=0 && image[nrow][ncol] == parent){
-                dfs(image,nrow,ncol,color,parent);
+            if(nrow < image.size() && nrow >= 0 && ncol < image[0].size() && ncol >=0 && image[nrow][ncol] == parent && !vis[nrow][ncol]){
+                dfs(image,vis,nrow,ncol,color,parent);
             }
         }
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
        int parent = image[sr][sc];
-       dfs(image,sr,sc,color,parent);
+       vector<vector<int>>vis(image.size(),vector<int>(image[0].size(),0));
+       dfs(image,vis,sr,sc,color,parent);
        return image;
     }
 };
