b3835ee0aab6983be9620a852f3b0bedb8e922d8
zeroTrace <142493670+0xZ15H4N@users.noreply.github.com>
2026-07-22 17:43:11 +0530

    0 ms (100%) 51.9 MB (84.29%) 07-22-2026 0802-find-eventual-safe-states Medium

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,35 +1,31 @@
 class Solution {
 private:
-    bool dfs(int start,vector<vector<int>>& graph,vector<int>&vis,vector<int>&pathVis,vector<int>&check_arr){
+    bool dfs(int start,vector<vector<int>>& graph,vector<int>&vis){
         vis[start] = 1;
-        pathVis[start] = 1;
         for(auto i : graph[start]){
-            if(!vis[i]){
-                if(dfs(i,graph,vis,pathVis,check_arr) == true){
+            if(vis[i] == 0){
+                if(dfs(i,graph,vis) == true){
                     return true;
                 }
-            }else if(pathVis[i]){
+            }else if(vis[i] == 1){
                 return true;
             }
         }
-        check_arr[start]=1;
-        pathVis[start] = 0;
+        vis[start] = 2;
         return false;
     }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int>vis(n,0);
-        vector<int>pathVis(n,0);
-        vector<int>check_arr(n,0);
         vector<int>ans;
         for(int i = 0 ; i < n;i++){
-            if(!vis[i]){
-                dfs(i,graph,vis,pathVis,check_arr);
+            if(vis[i]== 0){
+                dfs(i,graph,vis);
             }
         }
         for(int i = 0; i < n;i++){
-            if(check_arr[i]){
+            if(vis[i] == 2){
                 ans.push_back(i);
             }
         }
