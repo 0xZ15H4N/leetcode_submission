24effe2e1d12b1c4318197544aa794287d67024a
zeroTrace <142493670+0xZ15H4N@users.noreply.github.com>
2026-07-23 14:49:29 +0530

    306 ms (27.92%) 21.3 MB (52.99%) 07-23-2026 0127-word-ladder Hard

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord,
                      vector<string>& wordList) {
-        unordered_set<string>st(wordList.begin(),wordList.end());
-        if(st.find(endWord) == st.end()){
-            return 0;
-        }
         queue<pair<string, int>> q;
+        set<string> st(wordList.begin(), wordList.end());
+        if (st.find(endWord) == st.end()) {
+            return 0;
+        } // case endWord Does not exit
         q.push({beginWord, 1});
         while (!q.empty()) {
             string top = q.front().first;
-            int cnt = q.front().second;
+            int count = q.front().second;
             q.pop();
-            if (top == endWord) {
-                return cnt;
+           
+            if(top == endWord){
+                return count;
             }
             for (int i = 0; i < top.size(); i++) {
-                string temp = top;
-                for (int j = 0; j < 26; j++) {
-                    if(temp[i] == 'a' + j) {
-                        continue;
-                    }
-                    temp[i] = 'a' + j;
-                    if (st.find(temp) != st.end()) {
-                        q.push({temp, cnt + 1});
-                        st.erase(temp); // this is the important step in the process
-                    }
+                string temp  = top;
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    temp[i] = ch;
+                    if(st.find(temp) != st.end()){ //string exist
+                        q.push({temp,count+1});
+                        st.erase(temp);
+                    }  
                 }
             }
         }
